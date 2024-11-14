cc112574bb5bd0f2097113f52e33b44edccfcd88
shawn <shawn@ekshop2020.com>
2024-11-14 14:39:54 +0800

    feature/main: 1, 罗马数字转换；2，最后一个单词长度；3，公共首字符串

---
+++ src/main/java/com/longshanzhiyu/Array/leetcode58/Solution58.java
@@ -0,0 +1,17 @@
+package com.longshanzhiyu.Array.leetcode58;
+
+public class Solution58 {
+    public int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            System.out.print(s.charAt(index));
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
+}
---
+++ src/test/java/com/longshanzhiyu/Array/leetcode14/Solution14Test.java
@@ -0,0 +1,28 @@
+package com.longshanzhiyu.Array.leetcode14;
+
+import org.junit.jupiter.api.*;
+
+public class Solution14Test {
+
+    private Solution14 solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution14();
+    }
+
+    @Test
+    public void testCase1() {
+        String[] strs = {"flower","flow","flight"};
+        String expected = "fl";
+        Assertions.assertEquals(expected, solution.longestCommonPrefixIV(strs));
+    }
+
+    @Test
+    public void testCase2() {
+        String[] strs = {"dog","racecar","car"};
+        String expected = "";
+        Assertions.assertEquals(expected, solution.longestCommonPrefixIV(strs));
+    }
+
+}
